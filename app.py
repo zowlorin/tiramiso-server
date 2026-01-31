b55a2547923faafd7a66222769db6d11720d841b
xi-pec <xepicscripter@gmail.com>
2026-01-31 13:10:27 +0800

    feat: add list api

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from searcher import EmbeddedSearcher
+from searcher import EmbeddedSearcher, load_image_paths
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, url_for
 from markupsafe import escape
+import os
 
 SAMPLES_PATH = "static/items/"
 model = EmbeddedSearcher(SAMPLES_PATH)
 
 app = Flask(__name__, static_folder='static')
 
-@app.route("/api/search")
+@app.get("/api/search")
 def search():
     query = request.args.get("query", type=str)
     start = request.args.get("start", 0, type=int)
@@ -18,7 +19,14 @@ def search():
 
     items = model.query(query, start, count)
     formatted = [{ "path": path, "confidence": confidence } for path, confidence in items]
-    return jsonify(formatted)
+    return jsonify({ "items": formatted })
+
+
+@app.get("/api/list")
+def list():
+    files = load_image_paths(SAMPLES_PATH)
+    urls = [path.replace(os.sep, "/") for path in files]
+    return jsonify({ "items": urls })
 
 if __name__ == "__main__":
     app.run(port=3001)
